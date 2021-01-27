66357bb7ddf7cfac242167c35df653d36467fe6c
sielavic <63396872+sielavic@users.noreply.github.com>
2021-01-27 16:01:12 +0300

    Create Оптимизация_запроса.sql
    Оптимизация запроса. MySql

---
+++ Оптимизация_запроса.sql
@@ -0,0 +1,40 @@
+SELECT c.*,
+
+ COALESCE((select sum(width * height * length/10000)
+       from boxes b
+          where b.ID_CONTAINER = c.ID 
+          and b.ID_CONTAINER_MOVE = 0
+          and b.ID_STOCK = 1
+          and b.STATUS = 0
+          and b.NN_REZERV = 0
+          ), 0)
+          
+          +
+          
+          COALESCE((select sum(width * height * length/10000)
+       from boxes b
+          where b.ID_CONTAINER_MOVE = c.ID 
+          and b.ID_STOCK = 1
+          and b.STATUS = 0
+          and b.NN_REZERV = 0
+          ), 0) SUMMA
+ FROM containers c
+ WHERE c.STATUS = 0
+   AND COALESCE((select sum(width * height * length/10000)
+       from boxes b
+          where b.ID_CONTAINER = c.ID 
+          and b.ID_CONTAINER_MOVE = 0
+          and b.ID_STOCK = 1
+          and b.STATUS = 0
+          and b.NN_REZERV = 0
+          ) , 0)
+          +
+          COALESCE((select sum(width * height * length/10000)
+       from boxes b
+          where b.ID_CONTAINER_MOVE = c.ID 
+          and b.ID_STOCK = 1
+          and b.STATUS = 0
+          and b.NN_REZERV = 0
+          ) , 0)
+          
+          > 50
